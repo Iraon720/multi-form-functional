076d2b88b3e8dc324c77d7661cbd0b0d1213dc1b
Iraon720 <iraon3493@gmail.com>
2023-03-15 21:22:42 -0400

    CSS

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -3,19 +3,33 @@ import './SideNav.css';
 
 function SideNav() {
   return (
-    <div className='NavBar'>
-      <NavLink id='#step1' to='/'>
-        1
-      </NavLink>
-      <NavLink id='#step2' to='/plan'>
-        2
-      </NavLink>
-      <NavLink id='#step3' to='/add-ons'>
-        3
-      </NavLink>
-      <NavLink id='#step4' to='/summary'>
-        4
-      </NavLink>
+    <div className='sideNavContainer'>
+      <div className='stepNum'>
+        <div id='step1'>
+          <NavLink to='/'>1</NavLink>
+        </div>
+        <div id='step2'>
+          <NavLink id='#step2' to='/plan'>
+            2
+          </NavLink>
+        </div>
+        <section style={{ display: 'flex', flexDirection: 'row' }}>
+          <div id='step3'>
+            <NavLink id='#step3' to='/add-ons'>
+              3
+            </NavLink>
+          </div>
+          <div>
+            <p className='top'>Step 3</p>
+            <p className='bottom'>Add-Ons</p>
+          </div>
+        </section>
+        <div id='step4'>
+          <NavLink id='#step4' to='/summary'>
+            4
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
